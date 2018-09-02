809442a46fe6e92b2f0a60eb79004b3f11309311
iteration15 <dean@deankuhta.com>
2018-09-02 10:35:23 -0400

    updated norm_csv.py

--- norm_csv.py
+++ norm_csv.py
@@ -5,10 +5,8 @@ def normalize_file(infile, outfile):
     with open(infile, encoding='utf-8') as infile, open(outfile, "w", encoding='utf-8') as outfile:
         reader = csv.reader(infile, delimiter=',')
         writer = csv.writer(outfile)
-        headers = next(reader, None) #skip the headers
-
-        if headers:
-            writer.writerow(headers)
+        headers = next(reader, None) #don't parse headers
+        writer.writerow(headers) #write headers to new csv
 
         for row in reader:
             row[0] = datetime.datetime.strptime(row[0], "%m/%d/%y %H:%M:%S %p") + datetime.timedelta(hours=3)
@@ -17,7 +15,6 @@ def normalize_file(infile, outfile):
             row[4] = get_float(row[4])
             row[5] = get_float(row[5])
             row[6] = row[4] + row[5]
-
             writer.writerow(row)
 
 def get_float(t):
