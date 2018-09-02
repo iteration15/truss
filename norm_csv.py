ae3137a290446e7efb96bd716933da2ad82dbcfa
iteration15 <dean@deankuhta.com>
2018-09-02 01:08:56 -0400

    updated norm_csv.py :coffee:

--- norm_csv.py
+++ norm_csv.py
@@ -2,35 +2,28 @@ import csv
 import datetime
 
 def normalize_file(infile, outfile):
-    with open(infile, encoding='utf-8') as infile, open(outfile,"w") as outfile:
-        reader = csv.reader(infile)
-        headers = next(reader, None) #skip the headers
+    with open(infile, encoding='utf-8') as infile, open(outfile, "w", encoding='utf-8') as outfile:
+        reader = csv.reader(infile, delimiter=',')
         writer = csv.writer(outfile)
-
-        for row in reader:
-            timestamp = row[0]
-            print(timestamp)
-
+        headers = next(reader, None) #skip the headers
 
         if headers:
             writer.writerow(headers)
 
+        for row in reader:
+            row[0] = datetime.datetime.strptime(row[0], "%m/%d/%y %H:%M:%S %p") + datetime.timedelta(hours=3)
+            row[2] = row[2].zfill(5) #prepend with 0s until 5
+            row[3] = row[3].upper()
+            row[4] = get_float(row[4])
+            row[5] = get_float(row[5])
+            row[6] = row[4] + row[5]
 
+            writer.writerow(row)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def get_float(t):
+    hr, mm, sec = map(float, t.split(':'))
+    inMs = ((hr * 60 + mm) * 60 + sec) * 1000
+    return int(inMs)
 
 infile = input("Enter the file to normalize: ")
 outfile = input("Enter the normalized file name: ")
